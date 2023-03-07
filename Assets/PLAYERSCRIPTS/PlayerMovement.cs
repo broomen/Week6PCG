9714ea33d3299f05cce3b235942c57038bddfc4e
jbautista5435 <71132262+jbautista5435@users.noreply.github.com>
2023-03-07 11:07:19 -0500

    some changes

--- Assets/PLAYERSCRIPTS/PlayerMovement.cs
+++ Assets/PLAYERSCRIPTS/PlayerMovement.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 3.5f;
+    [SerializeField] private float rollSpeedMultiplier = 15f; 
     private Animator animator;
     private Vector2 input;
     private Vector2 lastInput;
@@ -18,23 +19,28 @@ public class PlayerMovement : MonoBehaviour
         input.y = Input.GetAxisRaw("Vertical");
         input.Normalize();
 
+        float currentSpeed = speed;
         if (Input.GetKeyDown(KeyCode.LeftShift)) // check if left shift key is pressed
         {
             if (input.y > 0) // up roll animation
             {
                 animator.SetTrigger("RollUp");
+                currentSpeed *= rollSpeedMultiplier;
             }
             else if (input.y < 0) // down roll animation
             {
                 animator.SetTrigger("RollDown");
+                currentSpeed *= rollSpeedMultiplier;
             }
             else if (input.x > 0) // right roll animation
             {
                 animator.SetTrigger("RollRight");
+                currentSpeed *= rollSpeedMultiplier;
             }
             else if (input.x < 0) // left roll animation
             {
                 animator.SetTrigger("RollLeft");
+                currentSpeed *= rollSpeedMultiplier;
             }
         }
         else // normal movement animation
@@ -48,7 +54,7 @@ public class PlayerMovement : MonoBehaviour
             animator.SetFloat("Speed", input.magnitude);
         }
 
-        transform.position += new Vector3(input.x, input.y) * speed * Time.deltaTime;
+        transform.position += new Vector3(input.x, input.y) * currentSpeed * Time.deltaTime;
     }
 
 }
